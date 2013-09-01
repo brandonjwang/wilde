869e0ee928af069ac12f728b601e95c64a4ae6a9
brwang <brandon@highfive.com>
2013-09-01 10:11:01 -0700

    blurriness

---
+++ wilde.js
@@ -0,0 +1,36 @@
+var Wilde = new function() {
+  this.canvasId = "main";
+  this.canvas = document.getElementById(this.canvasId);
+  this.ctx = this.canvas.getContext('2d');
+  this.colors = ["#797883", "#444E5A", "#999999", "#668899"];
+  this.getRandomColor = function() {
+    return this.colors[Math.floor(Math.random() * this.colors.length)];
+  }
+
+  this.drawCircle = function() {
+    this.ctx.beginPath();
+    this.ctx.arc(
+      Math.floor(Math.random() * this.canvas.width),
+      Math.floor(Math.random() * this.canvas.height),
+      Math.floor(Math.random() * (this.canvas.width + this.canvas.height)/10),
+      0, 2 * Math.PI, false
+    );
+    this.ctx.fillStyle = this.getRandomColor();
+    this.ctx.fill();
+    this.ctx.closePath();
+  };
+
+  this.actions = [this.drawCircle];
+  this.draw = function() {
+    this.canvas.width = document.width;
+    this.canvas.height = document.height;
+
+    var numActions = (this.canvas.width * this.canvas.height) / 100;
+    for (var i = 0; i < numActions; ++i) {
+      this.actions[Math.floor(Math.random() * this.actions.length)].call(this);
+    }
+    stackBlurCanvasRGB(this.canvasId, 0, 0, this.canvas.width, this.canvas.height, 170);
+  };
+}();
+
+Wilde.draw();
